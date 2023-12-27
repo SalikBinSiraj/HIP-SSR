43059684b35d66795704bc7abbf978b49d0fc01d
hassam-saeed <saeedhassam90@gmail.com>
2023-12-27 17:41:47 +0500

    More changes

--- renderer/_default.page.server.jsx
+++ renderer/_default.page.server.jsx
@@ -1,6 +1,6 @@
 export { render }
 // See https://vite-plugin-ssr.com/data-fetching
-export const passToClient = ['pageProps', 'urlPathname']
+export const passToClient = ['pageProps', 'urlPathname', 'routeParams']
 
 import ReactDOMServer from 'react-dom/server'
 import { PageShell } from './PageShell'
@@ -27,6 +27,9 @@ async function render(pageContext) {
       <head>
         <meta charset="UTF-8" />
         <link rel="icon" href="${logoUrl}" />
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" crossorigin="anonymous">
+        <meta property="og:image" content="https://phpstack-725513-2688800.cloudwaysapps.com/cms/wp-content/uploads/2022/11/logo512.png">
+        <meta name="theme-color" content="#000000" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta name="description" content="${desc}" />
         <title>${title}</title>
@@ -34,6 +37,15 @@ async function render(pageContext) {
       <body>
         <div id="react-root">${dangerouslySkipEscape(pageHtml)}</div>
       </body>
+      <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-W5TDXMD');</script>
+      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous" defer async></script>
+      <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-W5TDXMD"
+      height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+      <script src="https://www.hiparks.com/" integrity="sha384-Q3gPR+vy/ghJn/UKit+95NsbfEIa7f9QAOzcC3zaoHa577ETTRNZw0sSzghaYbuX" crossorigin="anonymous"></script>
     </html>`
 
   return {
